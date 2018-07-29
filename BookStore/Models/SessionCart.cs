f8a5c927fa7975844ad01935495f17c709441030
NickHe <nickyhe123@gmail.com>
2018-07-30 10:50:51 +1200

    test

--- BookStore/Models/SessionCart.cs
+++ BookStore/Models/SessionCart.cs
@@ -3,9 +3,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BookStore.Models
 {

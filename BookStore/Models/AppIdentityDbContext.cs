a39bc70faaeb66d86cfd818adee52eac545ef36a
NickHe <nickyhe123@gmail.com>
2018-07-30 11:12:45 +1200

    Create Identity Db

--- BookStore/Models/AppIdentityDbContext.cs
+++ BookStore/Models/AppIdentityDbContext.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookStore.Models
 {
-    public class AppIdentityDbContext
+    public class AppIdentityDbContext : IdentityDbContext<IdentityUser>
     {
-
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options)
+        : base(options) { }
     }
 }
